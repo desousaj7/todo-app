955299eb16116c58511f33c2b8072fe47f4c937d
desousaj7 <jhonnysousa@alu.ufc.br>
2018-12-18 00:36:49 -0200

    Incluído banco mongodb, node restful, as rotas e cors

---
+++ backend/src/config/server.js
@@ -0,0 +1,18 @@
+const port = 3003
+
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const server = express()
+const AllowCors = require('./cors')
+
+
+server.use(bodyParser.urlencoded({ extended: true}))
+server.use(bodyParser.json())
+server.use(AllowCors)
+
+server.listen(port, () => {
+    console.log(`BACKEND is running on port ${port}.`)
+})
+
+module.exports = server
