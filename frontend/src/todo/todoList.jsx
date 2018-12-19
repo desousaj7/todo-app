b30639afecb1f2cf7289c7d8b0f7a226ee4b2ff7
desousaj7 <jhonnysousa@alu.ufc.br>
2018-12-19 02:28:39 -0200

    Exclusão e refresh funcionando

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-    <div>
-        <h1>TodoList</h1>
-    </div>
-)
+export default props => {
+
+    const renderRows = () => {
+        const list = props.list || []
+        return list.map(todo => (
+            <tr key={todo._id}>
+                <td>{todo.description}</td>
+                <td>
+                    <IconButton style='danger' icon='trash'
+                        onClick={() => props.handleRemove(todo)}>
+                    </IconButton>
+                </td>
+            </tr>
+        ))
+    }
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Lista de tarefas</th>
+                    <th>Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
